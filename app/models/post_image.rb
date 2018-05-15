4c43c1976ddcd2d180e5a67e455b829794ee2fe4
Shota Watanabe <wtnbshut.mij@gmail.com>
2018-05-15 16:00:51 +0900

    Finish 5-4

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -1,5 +1,13 @@
 class PostImage < ApplicationRecord
 	attachment :image
 	belongs_to :user
+	has_many :favorites, dependent: :destroy
+	has_many :post_comments, dependent: :destroy
 
+	validates :shop_name, presence: true
+	validates :image, presence: true
+
+	def favorited_by?(user)
+		favorites.where(user_id: user.id).exists?
+	end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :post_images
+  has_many :post_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
 end
